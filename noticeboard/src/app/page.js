94bc0297203985272e0363cdbdc6a26ad57fc06a
donghyun kim <donghyun1998@naver.com>
2023-11-29 23:37:11 +0900

    add: db연결 추가

---
+++ noticeboard/src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { connectDB } from '@/util/database';
+import { MongoClient } from 'mongodb'
+
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("noticedb");
+  let a = await db.collection('post').find().toArray();
+  console.log(a);
+  return (
+    <div>안녕</div>
+  )
+}
